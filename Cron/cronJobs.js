147193afde3363ae82776992bde48bab7512194d
sanjaikannang <sanjaikannang@gmail.com>
2024-06-13 04:42:25 +0530

    13-06-24 morning commit

--- Cron/cronJobs.js
+++ Cron/cronJobs.js
@@ -7,7 +7,7 @@ export const scheduleCronJob = () => {
   // Schedule cron job to check for ended bids every minute
   cron.schedule('* * * * *', async () => {
     try {
-      // console.log('Checking for ended bids...');
+      console.log('Checking for ended bids...');
 
       // Find all products where bidding time has ended and bidding status is not 'Bidding Ended'
       const products = await Product.find({
@@ -17,11 +17,11 @@ export const scheduleCronJob = () => {
 
       // Iterate over products and send email to the winning bidder
       for (const product of products) {
-        // console.log(`Bidding time has ended for product ${product._id}. Sending email to the winning bidder...`);
+        console.log(`Bidding time has ended for product ${product._id}. Sending email to the winning bidder...`);
 
         // Check if highestBid and bidder fields are populated
         if (!product.highestBid || !product.highestBid.bidder) {
-          // console.error('Highest bid or bidder not found for product:', product._id);
+          console.error('Highest bid or bidder not found for product:', product._id);
           continue;
         }
 
@@ -33,7 +33,7 @@ export const scheduleCronJob = () => {
         await sendWinningBidEmail(product.highestBid.bidder.email, product);
       }
     } catch (error) {
-      // console.error('Error checking for ended bids:', error);
+      console.error('Error checking for ended bids:', error);
     }
   });
 };
