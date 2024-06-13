b921cf9ec7f1d593e7d2eba90d4c0c4c63be6e52
sanjaikannang <sanjaikannang@gmail.com>
2024-06-13 08:17:01 +0530

    13-06-24 morning commit

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -544,7 +544,18 @@ export const placeBid = async (req, res) => {
     
     // console.log(TotalBidAmount);
 
-    // console.log(typeof(product.highestBid.amount));       
+    // console.log(typeof(product.highestBid.amount));
+
+      // Check if bidding time has ended
+      if (currentTime > product.bidEndTime) {
+        // Update product bidding status to indicate bidding has ended
+        product.biddingStatus = "Bidding Ended";
+        await product.save();
+  
+        // Send email to the winning bidder
+        await sendWinningBidEmail(product.highestBid.bidder.email, product);
+      }
+  
 
     var TotalBidAmount = product.totalBidAmount;
 
@@ -587,15 +598,6 @@ export const placeBid = async (req, res) => {
     // Save the updated product to the database
     await product.save();
 
-     // Check if bidding time has ended
-     if (currentTime > product.bidEndTime) {
-      // Update product bidding status to indicate bidding has ended
-      product.biddingStatus = "Bidding Ended";
-      await product.save();
-
-      // Send email to the winning bidder
-      await sendWinningBidEmail(product.highestBid.bidder.email, product);
-    }
   
     // Respond with success message and the new bid
     res.status(201).json({
